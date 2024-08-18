40668f2c5c5fb9688edef7d705aec872b3420bcc
LeDucPr <124879680+LeDucPr@users.noreply.github.com>
2024-08-18 19:52:06 +0700

    khởi động bản V2 (mm2)

---
+++ MongoGE/AggregationsConf/Enums/EQueryOperators.cs
@@ -0,0 +1,50 @@
+﻿namespace MongoGE.AggregationsConf.Enums
+{
+    enum EComparisonOperator
+    {
+        Equal,
+        NotEqual,
+        GreaterThan,
+        GreaterThanOrEqual,
+        LessThan,
+        LessThanOrEqual
+    }
+    enum ELogicalOperator
+    {
+        And,
+        Or,
+        Not
+    }
+    enum ESetOperator
+    {
+        In,
+        NotIn,
+        All,
+        Size,
+        Exists
+    }
+    enum EElementOperator
+    {
+        Regex,
+        Type,
+    }
+    enum StageOperator
+    {
+        Match,
+        Project,
+        Group,
+        Sort,
+        Skip,
+        Limit,
+        Unwind,
+        Lookup,
+        AddFields,
+        ReplaceRoot,
+        Merge,
+        Bucket,
+        Sample,
+        Count,
+        Facet,
+        ConcatArrays
+    }
+}
---
+++ MongoGE/AggregationsConf/OperatorsAddTypes.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson;
+using EOType = MongoGE.AggregationsConf.Enums.EOperatorTypes;
+
+namespace MongoGE.AggregationsConf
+{
+    public class OperatorsAddTypes
+    {
+        private static Dictionary<EOType, BsonDocument> KeyTypesConf = new Dictionary<EOType, BsonDocument>
+        {
+            { EOType.Comparison, new BsonDocument { 
+                { "$eq", new BsonArray { BsonType.String, BsonType.Int32, BsonType.Double, BsonType.Boolean, BsonType.DateTime, BsonType.Array, BsonType.Document} }, 
+                { "$ne", new BsonArray { BsonType.String, BsonType.Int32, BsonType.Double, BsonType.Boolean, BsonType.DateTime, BsonType.Array, BsonType.Document} }, 
+                { "$gt", new BsonArray { BsonType.String, BsonType.Int32, BsonType.Double, BsonType.DateTime, BsonType.Array, BsonType.Document} }, 
+                { "$gte", new BsonArray { BsonType.String, BsonType.Int32, BsonType.Double, BsonType.DateTime, BsonType.Array, BsonType.Document} }, 
+                { "$lt", new BsonArray { BsonType.String, BsonType.Int32, BsonType.Double, BsonType.DateTime, BsonType.Array, BsonType.Document} }, 
+                { "$lte", new BsonArray { BsonType.String, BsonType.Int32, BsonType.Double, BsonType.DateTime, BsonType.Array, BsonType.Document} } }
+            },
+            { EOType.Logical, new BsonDocument { 
+                { "$and", BsonType.Array }, { "$or", BsonType.Array }, { "$not", BsonType.Array } } },
+            { EOType.Set, new BsonDocument { { "$in", BsonType.Array }, { "$nin", BsonType.Array }, { "$all", BsonType.Array }, { "$size", BsonType.Int32 }, { "$exists", BsonType.Boolean } } },
+            { EOType.Element, new BsonDocument { { "$regex", BsonType.String }, { "$type", BsonType.String } } },
+            { EOType.Stage, new BsonDocument { { "$match", BsonType.Document }, { "$project", BsonType.Document }, { "$group", BsonType.Document }, { "$sort", BsonType.Document }, { "$skip", BsonType.Int32 }, { "$limit", BsonType.Int32 }, { "$unwind", BsonType.String }, { "$lookup", BsonType.Document }, { "$addFields", BsonType.Document }, { "$replaceRoot", BsonType.Document }, { "$merge", BsonType.Document }, { "$bucket", BsonType.Document }, { "$sample", BsonType.Document }, { "$count", BsonType.String }, { "$facet", BsonType.Document }, { "$concatArrays", BsonType.Array } } }
+        };
+    }
+}
